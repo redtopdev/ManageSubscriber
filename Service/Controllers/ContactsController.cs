a31645cacc9dc0e3c37d00656a5154ca681d9db5
Mani0811 <mani.ciem@gmail.com>
2020-04-29 23:01:43 +0530

    modified to use engaze common web

--- Service/Controllers/ContactsController.cs
+++ Service/Controllers/ContactsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Register.DataContract;
 
@@ -23,8 +18,8 @@ namespace Register.Service.Controllers
             this.contactsManager = contactsManager;
         }
 
-        [HttpPost]
-        public IActionResult Post(ContactsRequest request)
+        [HttpGet]
+        public IActionResult Get(ContactsRequest request)
         {
             logger.LogInformation("Fetching Registered Contacts");
 
--- Service/Program.cs
+++ Service/Program.cs
@@ -4,46 +4,13 @@
 
 namespace Register.Service
 {
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Hosting;
-    using Serilog;
-    using System;
-
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .Build();
+            Engaze.Core.Web.EngazeWebHost.Run<Startup>(args);
 
-            Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(configuration)
-            .CreateLogger();
-
-            try
-            {
-                Log.Information("Application starting up. ");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)    
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
