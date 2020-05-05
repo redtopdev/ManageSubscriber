888e8936071066e0b4a2d68f0ee5166f369f2dca
Mani0811 <mani.ciem@gmail.com>
2020-05-06 01:38:07 +0530

    modified

--- Service/Startup.cs
+++ Service/Startup.cs
@@ -9,24 +9,20 @@ namespace Subscriber.Service
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Subscriber.DataPersistance;
-    using Serilog;
 
     public class Startup : EngazeStartup
     {
         public Startup(IConfiguration configuration) : base(configuration) { }
 
         public override void ConfigureComponentServices(IServiceCollection services)
-        {
-            services.AddControllers();
+        {         
             services.ConfigureCloudCassandra(Configuration);
             services.AddTransient<IProfileManager, ProfileManager>();
             services.AddTransient<IContactsManager, ContactsManager>();
             services.AddTransient<IUserProfileRepository, UserProfileRepository>();
         }
 
-        public override void ConfigureComponent(IApplicationBuilder app)
-        {
-            app.UseSerilogRequestLogging();
-        }
+        public override void ConfigureComponent(IApplicationBuilder app) { }
+        
     }
 }
